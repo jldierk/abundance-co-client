381c5f6768cb8a69deced6b10d7ccf75b55e5337
jldierk <jldierk@gmail.com>
2023-08-09 22:13:33 -0500

    Making the spinner better

--- src/components/ButtonWithLoad.jsx
+++ src/components/ButtonWithLoad.jsx
@@ -6,7 +6,7 @@ function ButtonWithLoad (props) {
     return (
         <div style={{display:"inline-block", alignItems: "center", justifyContent: "center"}}>
             {!loading && <button className="button btn-black-white" style={{width: "100%", height: "100%"}} onClick={() => props.onClickFunction()}>{props.buttonLabel}</button>}
-            {loading && <div className="spinner rotate" style={{height:props.height, width: props.width}}></div>}
+            {loading && <div className="spinner spinner-left rotate" style={{height:props.height, width: props.width}}></div>}
         </div>
     )
 
