76c40d207cb8fe3b10cdb361fc3ff738db433c6f
jldierk <jldierk@gmail.com>
2026-06-13 16:25:06 -0500

    updating site to be in an offline state

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { useNavigate } from "react-router-dom";
+import instagramLogo from "../assets/instagram.svg"
 
 export default function Home() {
     const [productArr, setProductArr] = useState([]);
@@ -32,16 +33,18 @@ export default function Home() {
     }
 
     return (
-        <div style={{backgroundColor: "var(--background)"}}>        
+        <div style={{backgroundColor: "var(--secondaryColor)", flex: 1, alignContent: "center"}}>        
             {/* <div className="info-block">Abundance Candles are hand-poured in Chicago, Illinois.</div> */}
             {/* <img src="src/assets/images/abundance-banner.jpg" style={{width: "100vw"}}></img>         */}
-            <div className="main-content">               
-                <div style={{display:"flex", width:"100%", justifyContent:"center"}}>
-                    <div style={{display: "flex", width:"910px", flexWrap:"wrap", justifyContent: "space-evenly", margin:"50px"}}>            
+            <div className="main-content" style={{backgroundColor: "var(--secondaryColor)"}}>               
+                <div style={{display:"flex", width:"100%", justifyContent:"center", textAlign:"center", color:"var(--navTextColor)", fontSize:"20px"}}>
+                    <b>Abundance Candle Co. is currently offline, but candles are still being poured! <br/>
+                    Reach out on Instagram @abundance_candle_co if you'd like to know more.</b>
+                    {/* <div style={{display: "flex", width:"910px", flexWrap:"wrap", justifyContent: "space-evenly", margin:"50px"}}>            
                         {productArr.filter(product=>product.activeInd == true)
                                     .map(product=><div key={product.id}><a onClick={()=>{toProductView(product)}}><ProductCard key={product.id} product={product}></ProductCard></a></div>)}            
-                    </div>
-                </div>
+                    </div> */}
+                </div>            
             </div>            
         </div>
     )
